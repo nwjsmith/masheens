12b13fc7b7858c64d268511d66a46cef649d21fd
Nate Smith <nate@theinternate.com>
2023-10-11 21:42:39 -0400

    Start to deduplicate

--- flake.nix
+++ flake.nix
@@ -11,60 +11,86 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = {
-    self,
-    nixpkgs-unstable,
-    nixos-unstable,
-    nix-darwin,
-    home-manager,
-  }: {
-    nixosConfigurations.dev = nixos-unstable.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs =
+    { self
+    , nixpkgs-unstable
+    , nixos-unstable
+    , nix-darwin
+    , home-manager
+    }:
+    let
+      forSystems = systems: f: nixpkgs-unstable.lib.genAttrs systems (system: f rec {
+        inherit system;
+        pkgs = import nixpkgs-unstable { inherit system; };
+      });
+      forAllSystems = forSystems [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      makeSystem =
+        { osSystem
+        , system
+        , cm
+        , hm
+        , user
+        , home
+        , hm-config
+        }: osSystem {
+          inherit system;
 
-      modules = [
-        ./configuration-dev.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nwjsmith = import ./home.nix;
-        }
-      ];
-    };
+          pkgs = import nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
 
-    nixosConfigurations.dev-vm = nixos-unstable.lib.nixosSystem {
-      system = "aarch64-linux";
-
-      modules = [
-        ./configuration-dev-vm.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nwjsmith = import ./home.nix;
-        }
-      ];
-    };
-
-    darwinConfigurations.nsmith0dae = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-
-      pkgs = import nixpkgs-unstable {
-        system = "aarch64-darwin";
-        config.allowUnfree = true;
+          modules = [
+            cm
+            hm
+            {
+              users.users.${user}.home = home;
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.nwjsmith = import hm-config;
+              };
+            }
+          ];
+        };
+    in
+    {
+      nixosConfigurations.dev = makeSystem {
+        osSystem = nixos-unstable.lib.nixosSystem;
+        system = "x86_64-linux";
+        cm = ./configuration-dev.nix;
+        hm = home-manager.nixosModules.home-manager;
+        user = "nwjsmith";
+        home = "/home/nwjsmith";
+        hm-config = ./home.nix;
       };
 
-      modules = [
-        home-manager.darwinModules.home-manager
-        ./darwin/darwin.nix
-        ({...}: {
-          users.users.nsmith.home = "/Users/nsmith";
-          home-manager = {
-            useGlobalPkgs = true;
-            users.nsmith = import ./darwin/home.nix;
+      nixosConfigurations.dev-vm = makeSystem {
+        osSystem = nixos-unstable.lib.nixosSystem;
+        system = "aarch64-linux";
+        cm = ./configuration-dev-vm.nix;
+        hm = home-manager.nixosModules.home-manager;
+        user = "nwjsmith";
+        home = "/home/nwjsmith";
+        hm-config = ./home.nix;
+      };
+
+      darwinConfigurations.nsmith0dae = makeSystem {
+        osSystem = nix-darwin.lib.darwinSystem;
+        system = "aarch64-darwin";
+        hm = home-manager.darwinModules.home-manager;
+        user = "nsmith";
+        home = "/Users/nsmith";
+        hm-config = ./darwin/home.nix;
+      };
+
+      devShells = forAllSystems ({ system, pkgs, ... }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nixpkgs-fmt
+            ];
           };
-        })
-      ];
+        });
     };
-  };
 }
