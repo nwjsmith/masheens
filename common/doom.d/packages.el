a7165aaa6b2288a9463c28119bd066531b846d3c
Nate Smith <nate@theinternate.com>
2024-03-19 15:39:46 -0400

    Trying out Bazel
    This is how bad things have gotten

--- common/doom.d/packages.el
+++ common/doom.d/packages.el
@@ -1,6 +1,8 @@
-(package! jest-test-mode)
-(package! evil-cleverparens)
-(package! jarchive)
-(package! modus-themes)
+(package! bazel-mode
+  :recipe (:host github :repo "bazelbuild/emacs-bazel-mode"))
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+(package! evil-cleverparens)
+(package! jarchive)
+(package! jest-test-mode)
+(package! modus-themes)
