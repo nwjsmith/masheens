73f1e22aa7824e917e0f3af399dd8b088e2745ea
Nate Smith <nate@theinternate.com>
2024-11-13 22:25:16 -0500

    GPG key signing can't be done concurrently
    The only other reason I'd use it (I think) is nostalgia? SSH key signing
    seems strictly better.

--- common/home-configuration.nix
+++ common/home-configuration.nix
@@ -65,8 +65,8 @@
         ];
       };
       signing = {
-        backend = "gpg";
-        key = "nate@theinternate.com";
+        backend = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtWR1nXAvSmsd92TC9rMuZIh1Ec8cqxYr3BIyUxdNyy";
         sign-all = true;
       };
       ui = {
