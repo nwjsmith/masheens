ef03ad74d8c48386810bd7adb37136e38c909231
Nate Smith <nate@theinternate.com>
2024-12-20 13:56:55 -0500

    Fix SSH commit signing

--- common/git.nix
+++ common/git.nix
@@ -26,10 +26,12 @@
       };
       fetch.prune = true;
       github.user = "nwjsmith";
+      gpg.format = "ssh";
       init.defaultBranch = "main";
-      merge.conflictStyle = "diff3";
+      merge.conflictStyle = "zdiff3";
       pull.rebase = true;
       push.default = "current";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtWR1nXAvSmsd92TC9rMuZIh1Ec8cqxYr3BIyUxdNyy";
       rebase = {
         autoSquash = true;
         autoStash = true;
