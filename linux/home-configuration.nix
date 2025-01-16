906adc744bdd608631f6927b981d0fe90a542dae
Nate Smith <nate@theinternate.com>
2025-01-16 13:29:19 -0500

    Consolidate Ghostty config

--- linux/home-configuration.nix
+++ linux/home-configuration.nix
@@ -25,46 +25,7 @@
     package = pkgs.firefox-wayland;
   };
 
-  programs.ghostty = {
-    enable = true;
-    settings = {
-      background = "#ffffff";
-      cursor-color = "#000000";
-      cursor-text = "#ffffff";
-      font-family = "JetBrains Mono";
-      font-feature = "-calt";
-      font-size = 11;
-      foreground = "#000000";
-      palette = [
-        # black
-        "0=#000000"
-        "8=#595959"
-        # red
-        "1=#a60000"
-        "9=#972500"
-        # green
-        "2=#006800"
-        "10=#00663f"
-        # yellow
-        "3=#6f5500"
-        "11=#884900"
-        # blue
-        "4=#0031a9"
-        "12=#3548cf"
-        # purple
-        "5=#721045"
-        "13=#531ab6"
-        # aqua
-        "6=#005e8b"
-        "14=#005f5f"
-        # white
-        "7=#a6a6a6"
-        "15=#ffffff"
-      ];
-      selection-foreground = "#000000";
-      selection-background = "#bdbdbd";
-    };
-  };
+  programs.ghostty.settings.font-size = 11;
 
   fonts.fontconfig.enable = true;
 
